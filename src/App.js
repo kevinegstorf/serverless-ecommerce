a380078ca23cf800fb46c74c1e3454683ef89684
kevinegstorf <kevin.egstorf@gmail.com>
2018-11-19 11:29:57 +0100

    update opn routing

--- src/App.js
+++ src/App.js
@@ -1,15 +1,19 @@
 import React, { Component } from 'react';
 import { BrowserRouter, Router, Route, Switch, Link} from "react-router-dom";
+
 import HomePage from './pages/Homepage/HomePage';
 import ProductsPage from './pages/ProductsPage/ProductsPage';
 
 export default class  App extends Component {
   render() {
     return (
-      <Switch>
-        <Route exact path="/" component={HomePage}/>
-        <Route path="/products" component={ProductsPage}/>
-      </Switch>
-    );
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/" component={HomePage}/>
+          <Route path="/products" component={ProductsPage}/>
+        </Switch>
+      </BrowserRouter>
+      );
+
   }
 }
