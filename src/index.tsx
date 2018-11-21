44e53262f0874e44dd363d4ec75fafd0d0e6b4c3
kevinegstorf <kevin.egstorf@gmail.com>
2018-11-21 13:35:23 +0100

    added global styling :art:

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { injectGlobal } from 'emotion'
+
+injectGlobal`
+  html,body{
+    font-family: 'Lato';
+  }
+`;
 
 ReactDOM.render(<App />, document.getElementById('root'));
 
