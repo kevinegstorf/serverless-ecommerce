f7b7311e787207c10f940953af292475c27e3632
kevinegstorf <kevin.egstorf@gmail.com>
2018-11-23 15:01:04 +0100

    button component updates 🚧

--- src/components/Button/Button.stories.tsx
+++ src/components/Button/Button.stories.tsx
@@ -2,13 +2,12 @@ import React from 'react';
 import { withInfo } from "@storybook/addon-info";
 
 import { storiesOf } from '@storybook/react';
-import { Button } from './Button';
+import Button from './Button';
 import { text } from '@storybook/addon-knobs/react';
+import withTests from '../../withTests';
 
 (storiesOf('Button', module) as any)
+.addDecorator(withTests('Button'))
   .addWithJSX(
-    'Button Info', withInfo({ inline: true })(() => <Button onClick={() => {}}>{text('text', 'Click Me')}</Button>)
-  )
-  .addWithJSX('example default', () => (
-    <Button onClick={() => {}}>{text('text', 'Click Me')}</Button>
-  ));
+    'Button Default', withInfo({ inline: true })(() => <Button onClick={() => {}}>{text('text', 'Click Me')}</Button>)
+  );
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -4,7 +4,35 @@ import { css } from 'emotion';
 
 const button = css`
   background-color: ${color.pink};
-  font-family: ${font.main};
+  font-family: ${font.button};
+  min-width: 150px;
+  min-height: 40px;
+  padding: 0;
+  border: none;
+  border-radius: 5px;
+  transition-duration: 0.2s;
+  box-shadow: 0 5px 11px rgba(33,33,33,.2); 
+  
+  &:hover {
+    transition-duration: 0.3s;
+    box-shadow: 0 19px 36px rgba(33,33,33,.2);
+    background-color: deeppink; 
+    cursor: pointer;
+  }
+
+  &:focus {
+    outline:none
+  }
+
+  &:active {
+    transition-duration: 0.1s;
+    box-shadow: 0 5px 11px rgba(33,33,33,.2);
+  }
+`;
+
+const buttonSpan= css`
+  font-size: 20px;
+  color: white;
 `;
 
 export interface Props {
@@ -15,16 +43,18 @@ export interface Props {
   /**
    * Disables onclick
    *
-   * @default false
+   * @default no
    **/
   disabled?: boolean;
 }
 const noop = () => {};
-export const Button = (props: Props) => {
+const Button = (props: Props) => {
   const { children, onClick, disabled = false } = props;
   return (
     <button className={button} onClick={!disabled ? onClick : noop}>
-      <span>{children}</span>
+      <span className={buttonSpan}>{children}</span>
     </button>
   );
 };
+
+export default Button;
