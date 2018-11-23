490ef37eec55c5569aaddc62bf9e4b4f6d3984ea
kevinegstorf <kevin.egstorf@gmail.com>
2018-11-23 10:49:55 +0100

    added 404 page

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,20 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route, Switch} from "react-router-dom";
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 
 import HomePage from './pages/Hompage/Homepage';
 import ProductsPage from './pages/ProductPage/ProductPage';
+import FourOFour from './pages/FourOFour/FourOFour';
 
-export default class  App extends Component {
+export default class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Switch>
-          <Route exact path="/" component={HomePage}/>
-          <Route path="/products" component={ProductsPage}/>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/products" component={ProductsPage} />
+          <Route component={FourOFour} />
         </Switch>
       </BrowserRouter>
-      );
+    );
   }
 }
---
+++ src/pages/FourOFour/FourOFour.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+interface FourOFour {}
+
+const FourOFour: React.SFC<FourOFour> = props => {
+  return <div>404 whoops something went wrong</div>;
+};
+
+export default FourOFour;
