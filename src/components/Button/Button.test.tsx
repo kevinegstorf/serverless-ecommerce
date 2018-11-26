987f111a545a85f23219446cf350a01210740f3f
kevinegstorf <kevin.egstorf@gmail.com>
2018-11-26 10:15:46 +0100

    added click event test 🔬

--- src/components/Button/Button.test.tsx
+++ src/components/Button/Button.test.tsx
@@ -1,8 +1,19 @@
 import React from 'react';
-import { render } from 'react-testing-library';
+import { render, cleanup, fireEvent } from 'react-testing-library';
 import Button from './Button';
 
-test('should render', () => {
-    const container = render(<Button onClick={() => {}}>Click me</Button>)
+afterEach(cleanup)
+
+test('button snapshot', () => {
+    const container  = render(<Button onClick={() => {}}>Click me</Button>)
+
     expect(container).toMatchSnapshot();
+});
+
+test('button click', () => {
+    const handleClick = jest.fn()
+    const {getByText}  = render(<Button onClick={handleClick}>Click me</Button>)
+
+    fireEvent.click(getByText('Click me'))
+    expect(handleClick).toHaveBeenCalledTimes(1)
 });
